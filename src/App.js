9d28e7c3b7beaabfae3508ef7df75f6526ba8d6f
Berk Bozok <berk_bozok@live.com>
2022-05-27 10:45:07 -0400

    social butttons added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.scss';
+
 
 function App() {
   return (
@@ -7,10 +9,18 @@ function App() {
       <header className="App-header">
         
       <div class="title">
- <h1> Berk Bozok</h1>
+ 
 
 
-
+ <div class="social-container">
+    <ul class="social-icons">
+    <h1> Berk Bozok</h1>
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+    </ul>
+    </div>
 
  <a target="_blank" href="https://github.com/berkbozok" class="white-mode">Github</a>
 </div>
