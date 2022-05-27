9d0a039958212e8fec58b4cf7d7e5ef8c0e53de2
Berk Bozok <berk_bozok@live.com>
2022-05-27 11:00:32 -0400

    social button aligned

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import './SocialButton.scss';
+import Linkedin from './components/Linkedin';
 
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
-      <div class="title">
- 
+
+        <div class="title">
 
 
- <div class="social-container">
-    <ul class="social-icons">
-    <h1> Berk Bozok</h1>
-        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-        <li><a href="https://www.linkedin.com/in/berk-bozok-512998168/"><i class="fa fa-linkedin"></i></a></li>
-        <li><a href="#"><i class="fa fa-codepen"></i></a></li>
-    </ul>
-    </div>
 
- <a target="_blank" href="https://github.com/berkbozok" class="white-mode">Github</a>
-</div>
-      
+          <div class="social-container">
+            <ul class="social-icons">
+              <h1> Berk Bozok</h1>
+              <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><Linkedin /></li>
+              <li><a href="#"><i class="fa fa-codepen"></i></a></li>
+            </ul>
+          </div>
+
+          <a target="_blank" href="https://github.com/berkbozok" class="white-mode">Github</a>
+        </div>
+
       </header>
     </div>
   );
---
+++ src/components/Linkedin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Linkedin() {
+    return (
+        <>  
+       <a href="https://www.linkedin.com/in/berk-bozok-512998168/"><i class="fa fa-linkedin"></i></a>  </>
+     
+  
+    );
+  }
+  export default Linkedin;
