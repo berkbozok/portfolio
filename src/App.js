cacc87272a7aeca564b4b4bc978b6ea0f07fb178
Berk Bozok <berk_bozok@live.com>
2022-05-30 14:43:21 -0400

    new buttons added

--- src/App.js
+++ src/App.js
@@ -8,12 +8,24 @@ import Name from './components/Name';
 
 const App = () => {
   return (
+    
       <header className="App-header">
+             <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"></link>
+<nav class="social">
+  
+ 
+          <ul>
+              <li><a href="https://twitter.com/highflyer910">Twitter <i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/highflyer910">Github <i class="fa fa-github"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/teonamushambadze">Linkedin <i class="fa fa-linkedin"></i></a></li>
+              </ul>
+      </nav>
 
         <div class="title">
 
           <div class="social-container">
             <ul class="social-icons">
+       
               <Name/>
               <li><Facebook/></li>
               <li><Linkedin /></li>
