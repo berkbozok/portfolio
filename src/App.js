3e612328bb9024f1e2955b79ea6a2360b9bab16b
Berk Bozok <berk_bozok@live.com>
2022-06-01 00:41:06 -0400

    deleted component

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ import Linkedin from './components/Linkedin';
 import Github from './components/Github';
 import Facebook from './components/Facebook';
 import Name from './components/Name';
+import Sidebar from './components/Sidebar';
+
 
 const App = () => {
   return (
     
       <header className="App-header">
-             <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet"></link>
+             
 <nav class="social">
-          <ul>
-              <li><a href="https://twitter.com/highflyer910">Twitter <i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/highflyer910">Github <i class="fa fa-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/teonamushambadze">Linkedin <i class="fa fa-linkedin"></i></a></li>
-              </ul>
+          
       </nav>
 
         <div class="title">
 
           <div class="social-container">
+            
             <ul class="social-icons">
        
               <Name/>
@@ -30,6 +29,7 @@ const App = () => {
               <li><Github/></li>
               <li><a href="#"><i class="fa fa-info"></i></a></li>
             </ul>
+            
           </div>
 
          
