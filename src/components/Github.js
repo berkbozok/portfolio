3ee14c6ad49a763d4120b821d1b5d30e8a0e714e
Berk Bozok <berk_bozok@live.com>
2022-06-10 23:58:08 -0400

    github refactor

--- src/components/Github.js
+++ src/components/Github.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Github() {
     return (
         <>  
-        <a href="https://github.com/berkbozok"><i class="fa fa-github"></i></a>
+        <a href="https://github.com/berkbozok"><i class="fa fa-github"></i>
+        </a>
         </>
     );
   }
