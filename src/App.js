d32159640024ab7598d9a3edc4cfae05f8568623
Berk Bozok <berk_bozok@live.com>
2022-06-21 22:55:17 -0400

    readable code

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ import Facebook from './components/Facebook';
 import Name from './components/Name';
 //import Sidebar from './components/Sidebar';
 import Info from './components/Info';
-
 //import List from './components/List';
 //import TitleAndButton from './components/TitleAndButton';
-
 
 const App = () => {
   return (
