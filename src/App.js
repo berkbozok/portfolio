8b643e18eaf7c1fa23af62fc9f423d33409c8255
Berk Bozok <berk_bozok@live.com>
2022-06-22 15:50:13 -0400

    redo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import './App.css';
 import './SocialButton.scss';
-import Linkedin from './components/Linkedin';
-import Github from './components/Github';
-import Facebook from './components/Facebook';
+
 import Name from './components/Name';
-//import Sidebar from './components/Sidebar';
-import Info from './components/Info';
-//import List from './components/List';
-//import TitleAndButton from './components/TitleAndButton';
+
 
 const App = () => {
   return (
 
     <header className="App-header">
 
-      <nav class="social">
+      
+      <Name/>
 
-      </nav>
-      <ul class="social-icons">
-
-        <Name/>
-        <li><Facebook /></li>
-        <li><Linkedin /></li>
-        <li><Github /></li>
-        <li><Info /></li>
-
-      </ul>
-
-      <button class="more-button" onclick="https://github.com/berkbozok/login-demo">
-        <div></div>
-      </button >
+   
 
     </header>
 
