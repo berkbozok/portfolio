50b650d830e95d181230edd25c91e692803227c4
Berk Bozok <berk_bozok@live.com>
2022-06-23 21:59:29 -0400

    refactoring

--- src/components/Name.js
+++ src/components/Name.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Name() {
     return (
-        <>  
-       <h1> Berk Bozok</h1>
-        </>
+    <>  
+       <h1 class="header-1"> Berk Bozok </h1>
+    </>
     );
   }
   export default Name;
